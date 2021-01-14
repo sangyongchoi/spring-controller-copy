30089a95dd4a59385bbe61922ff2e961462f9775
sangyongchoi <csy7792@naver.com>
2021-01-15 02:18:54 +0900

    refactoring

--- src/main/java/flow/filter/HandlerMapping.java
+++ src/main/java/flow/filter/HandlerMapping.java
@@ -9,6 +9,7 @@ import flow.annotation.RestController;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,32 +42,12 @@ public class HandlerMapping {
                             if (aClass.isAnnotationPresent(Controller.class)) {
                                 Method[] methods = aClass.getMethods();
                                 for (Method m : methods) {
-                                    if (m.isAnnotationPresent(PostMapping.class)) {
-                                        PostMapping declaredAnnotation = m.getDeclaredAnnotation(PostMapping.class);
-                                        String value = declaredAnnotation.value();
-                                        MethodInvoker methodInvoker = new MethodInvoker(value, MethodType.PAGE, aClass, m);
-                                        controller.put(value, methodInvoker);
-                                    } else if (m.isAnnotationPresent(GetMapping.class)) {
-                                        GetMapping declaredAnnotation = m.getDeclaredAnnotation(GetMapping.class);
-                                        String value = declaredAnnotation.value();
-                                        MethodInvoker methodInvoker = new MethodInvoker(value, MethodType.PAGE, aClass, m);
-                                        controller.put(value, methodInvoker);
-                                    }
+                                    addPageHandler(aClass, m);
                                 }
                             } else if (aClass.isAnnotationPresent(RestController.class)) {
                                 Method[] methods = aClass.getMethods();
                                 for (Method m : methods) {
-                                    if (m.isAnnotationPresent(PostMapping.class)) {
-                                        PostMapping declaredAnnotation = m.getDeclaredAnnotation(PostMapping.class);
-                                        String value = declaredAnnotation.value();
-                                        MethodInvoker methodInvoker = new MethodInvoker(value, MethodType.POST, aClass, m);
-                                        restController.put(value, methodInvoker);
-                                    } else if (m.isAnnotationPresent(GetMapping.class)) {
-                                        GetMapping declaredAnnotation = m.getDeclaredAnnotation(GetMapping.class);
-                                        String value = declaredAnnotation.value();
-                                        MethodInvoker methodInvoker = new MethodInvoker(value, MethodType.GET, aClass, m);
-                                        restController.put(value, methodInvoker);
-                                    }
+                                    addRestHandler(aClass, m);
                                 }
                             }
                         }
@@ -77,6 +58,46 @@ public class HandlerMapping {
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void addPageHandler(Class<?> aClass, Method m) {
+        if (m.isAnnotationPresent(PostMapping.class)) {
+            addPostHandler(aClass, m, MethodType.PAGE);
+        } else if (m.isAnnotationPresent(GetMapping.class)) {
+            addPostHandler(aClass, m, MethodType.PAGE);
+        }
+    }
+
+    private void addRestHandler(Class<?> aClass, Method m) {
+        if (m.isAnnotationPresent(PostMapping.class)) {
+            addPostHandler(aClass, m, MethodType.POST);
+        } else if (m.isAnnotationPresent(GetMapping.class)) {
+            addGetMapping(aClass, m, MethodType.GET);
+        }
+    }
+
+    private void addPostHandler(Class<?> aClass, Method m, MethodType methodType){
+        PostMapping declaredAnnotation = m.getDeclaredAnnotation(PostMapping.class);
+        String value = declaredAnnotation.value();
+        MethodInvoker methodInvoker = new MethodInvoker(value, methodType, aClass, m);
+
+        if(MethodType.PAGE.equals(methodType)){
+            controller.put(value, methodInvoker);
+        }else{
+            restController.put(value, methodInvoker);
+        }
+    }
+
+    private void addGetMapping(Class<?> aClass, Method m, MethodType methodType){
+        GetMapping declaredAnnotation = m.getDeclaredAnnotation(GetMapping.class);
+        String value = declaredAnnotation.value();
+        MethodInvoker methodInvoker = new MethodInvoker(value, methodType, aClass, m);
+
+        if(MethodType.PAGE.equals(methodType)){
+            controller.put(value, methodInvoker);
+        }else{
+            restController.put(value, methodInvoker);
         }
     }
 }
